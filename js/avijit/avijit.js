cddfdeda711f952a1190807c9025bbf1e2a803dd
AvijitAcharjee <avijitach@gmail.com>
2021-05-16 11:16:54 +0600

    snackbar added

--- js/avijit/avijit.js
+++ js/avijit/avijit.js
@@ -20,6 +20,7 @@ $('#contact_submit').click(function (e) {
             success: function(responseData, textStatus, jqXHR) {
                 var value = responseData.responseText;
                 console.log(value);
+                showSnackbar("Message sent successfully",3000);
             },
             error: function (responseData, textStatus, errorThrown) {
                 console.log(errorThrown);
@@ -30,3 +31,13 @@ $('#contact_submit').click(function (e) {
         // console.log(JSON.stringify(d, null, 2));
     });
 });
+$('#hire_me_now').click(function(e) {
+    showSnackbar("Please send a message in contact section to continue", 4000);
+})
+
+function showSnackbar(message,time) {
+    var x = document.getElementById("snackbar");
+    x.innerText = message;
+    x.className = "show";
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, time);
+}
